8ac3c1b0d9d5ab2723c8e0ad442d37f332e41a65
Alon Shorr <alon1shorr@gmail.com>
2025-06-13 14:40:01 +0300

    FINAL UPLOAD FOR HAFALEYLEY

--- 3.Custom_Allocator/customAllocator.h
+++ 3.Custom_Allocator/customAllocator.h
@@ -1,19 +1,33 @@
 #ifndef CUSTOM_ALLOCATOR
 #define CUSTOM_ALLOCATOR
-
+//
 /*=============================================================================
 * Do no edit lines below!
 =============================================================================*/
+#include <unistd.h> //for sbrk, brk
+#include <stdint.h> //for uintptr_t
+#include <stdlib.h> //for malloc, free, calloc, realloc
+#include <stdio.h>  //for printf, fprintf
+#include <string.h> //for memset, memcpy
+#include <errno.h>  //for errno
 #include <stddef.h> //for size_t
+#include <stdbool.h> //for bool
+
+
 
 void* customMalloc(size_t size);
 void customFree(void* ptr);
 void* customCalloc(size_t nmemb, size_t size);
 void* customRealloc(void* ptr, size_t size);
 
+
 /*=============================================================================
 * Do no edit lines above!
 =============================================================================*/
+
+
+
+
 // Heap creation and destruction functions - 
 void heapCreate()
 {
@@ -45,13 +59,27 @@ void heapKill()
 #define ALIGN_TO_MULT_OF_4(x) (((((x) - 1) >> 2) << 2) + 4)
 
 /*=============================================================================
-* Block
+* Block 
 =============================================================================*/
 // Suggestion for block usage - feel free to change this
 typedef struct Block {
     size_t size;
-    bool free;
-    Block* next;
+    //bool free;
+    struct Block* next;
 } Block;
+
+
+
+/*=============================================================================
+* added functions
+=============================================================================*/
+int brk(void *end_data_segment);
+void handleSbrkError(void *sbrk_result);
+void handlebrkError(int brk_result);
+bool is_valid_malloc_ptr (void *ptr);
+Block* find_free_place(size_t size);
+//inline Block *payload_to_block(void *payload);
+//inline void *block_to_payload(Block *b);
+
 
 #endif // CUSTOM_ALLOCATOR
